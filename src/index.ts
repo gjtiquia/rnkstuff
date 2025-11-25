207be6ac57237d86e105c29862b2fd05e2347fb4
GJ Tiquia <gershom.tiquia@gmail.com>
2025-11-25 00:51:00 -0500

    failed attempt to give text to Bun via stdin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-console.log("Hello World!")
 
-const firstName = prompt("what is your name?")
+main();
 
-console.log(`your name is ${firstName}`)
+async function main(): Promise<Status> {
+    console.log("rnkstuff")
 
-const result = confirm("are you sure?")
+    // const stdinSize = Bun.stdin.size
+    // console.log(stdinSize)
+    // if (isNaN(stdinSize) || !isFinite(stdinSize)) { // no input gives infinite stdin.size
+    //     console.error("no input file detected! try doing something like `cat file.txt | bun start")
+    //     return "ERROR";
+    // }
 
-console.log(result)
+    const inputFile = await Bun.stdin.text()
 
-alert("yoooo this is an alert")
+    console.log(inputFile)
+
+    const stuffToRank = inputFile.split("\n")
+    if (stuffToRank.length <= 0) {
+        console.error("no input file detected! try doing something like `cat file.txt | bun start")
+        return "ERROR";
+    }
+
+    console.log(stuffToRank)
+
+    const firstName = prompt("what is your name?")
+
+    console.log(`your name is ${firstName}`)
+
+    const result = confirm("are you sure?")
+
+    console.log(result)
+
+    alert("yoooo this is an alert")
+    return "OK"
+}
+
+type Status = "OK" | "ERROR"
